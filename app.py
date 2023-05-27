fe09cdc89f627b56408ce70adf7836cf078fd137
Mark Wiemer <markwiemer@outlook.com>
2023-05-26 18:17:59 -0700

    Use Turbo, not Davinci

--- app.py
+++ app.py
@@ -11,12 +11,15 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         animal = request.form["animal"]
-        response = openai.Completion.create(
+        response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            prompt=generate_prompt(animal),
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": f"{generate_prompt(animal)}"},
+            ],
             temperature=0.6,
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        return redirect(url_for("index", result=response.choices[0].message.content))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
